5909be01b42abbe309e923f67ce567061e3f82f2
Arijit Khan <arijit.khan@broadifitech.com>
2026-07-17 11:39:40 +0530

    feat: initialize project assets, constants, types, utilities, and apply styled safe areas to tabs

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -1,64 +1,26 @@
+import { HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
 import "@/global.css";
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+import { Image, StatusBar, Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text className="text-xl font-bold text-muted-foreground">
-        Welcome to Nativewind!
-      </Text>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <StatusBar barStyle="dark-content" />
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
 
-      <View>
-        <Link
-          href="/onboarding"
-          className="mt-4 rounded-2xl px-3 py-2 bg-foreground text-white mx-auto my-auto"
-        >
-          <Text>Go to Onboarding</Text>
-        </Link>
-
-        <Link
-          href="/(auth)/sign-in"
-          className="mt-4 rounded-2xl px-3 py-2 bg-foreground text-white mx-auto"
-        >
-          <Text>Go to SignIn</Text>
-        </Link>
-
-        <Link
-          href="/(auth)/sign-up"
-          className="mt-4 rounded-2xl px-3 py-2 mx-auto bg-foreground text-white"
-        >
-          <Text>Go to SignUp</Text>
-        </Link>
-
-        {/* Subscription Page */}
-        <Link
-          href="/subscriptions/spotify"
-          className="mt-4 rounded-2xl px-3 py-2 mx-auto bg-foreground text-white"
-        >
-          <Text>Spotify Subscription</Text>
-        </Link>
-
-        <Link
-          href={{
-            pathname: "/subscriptions/[id]",
-            params: { id: "youtube" },
-          }}
-          className="mt-4 rounded-2xl px-3 py-2 mx-auto bg-foreground text-white"
-        >
-          <Text>Youtube Premium Subscription</Text>
-        </Link>
+        <View className="rounded-full overflow-hidden p-1 border border-gray-200">
+          <Image source={icons.add} className="home-add-icon" />
+        </View>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFF9E3",
-  },
-});
